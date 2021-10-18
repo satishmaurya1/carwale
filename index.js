89a754f5d1d6a1874dc9304d4400d7874349fd02
Satish maurya <satishmaurya15216@gmail.com>
2021-10-18 22:20:14 +0530

    featured section added

--- index.js
+++ index.js
@@ -99,3 +99,38 @@ var swiper = new Swiper(".vehicles-slider", {
       },
     },
   });
+
+
+
+  // ---------------featured--------------------
+
+  var swiper = new Swiper(".featured-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+       
+      },
+      1040: {
+        slidesPerView: 3,
+        
+      },
+    },
+  });
