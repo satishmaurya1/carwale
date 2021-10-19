a13724818ddb09e8909d0047ccf515fa7b64f4ed
Satish maurya <satishmaurya15216@gmail.com>
2021-10-19 22:52:58 +0530

    footer added

--- index.js
+++ index.js
@@ -137,6 +137,38 @@ var swiper = new Swiper(".vehicles-slider", {
 
 
 
-  // --------------------news letter---------------------
+  // -----------------reviews-----------------
+
+
+  var swiper = new Swiper(".reviews-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+       
+      },
+      1040: {
+        slidesPerView: 3,
+        
+      },
+    },
+  });
 
 
